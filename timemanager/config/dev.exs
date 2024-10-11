d4940c9e6a84dd02a9f0598d2c7925fa36a97bf6
Darkbuilder646 <theolejeune.tl@gmail.com>
2024-10-11 14:32:41 +0200

    Fix: docker file back et compose

--- timemanager/config/dev.exs
+++ timemanager/config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :timemanager, Timemanager.Repo,
   username: "admin",
   password: "admin",
-  hostname: "localhost",
+  hostname: "db",
   database: "timemanager_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,

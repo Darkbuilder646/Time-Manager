fe7389213aa595cfbea47923b541b3a4d4d1cc2e
Darkbuilder646 <theolejeune.tl@gmail.com>
2024-10-15 10:44:53 +0200

    update CI front + add CI back

--- timemanager/config/test.exs
+++ timemanager/config/test.exs
@@ -2,16 +2,23 @@ import Config
 
 # Configure your database
 #
-# The MIX_TEST_PARTITION environment variable can be used
+# The MIX_TEST environment variable can be used
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :timemanager, Timemanager.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "timemanager_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "timemanager_#{System.get_env("MIX_TEST")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, App.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
